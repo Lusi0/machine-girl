e3d7ebb44f8dfe84dc3fdbb221bb41aa78aeba0a
Lusi0 <ItsLusio@gmail.com>
2022-03-19 13:23:47 -0700

    added cards
    Added Machine Girl 01
    Added hounds of the hellscape 27
    Added Hellscape Tour 35
    Changed Wolf Girl picture from https://i.imgur.com/JJSVvp8.png -> https://i.imgur.com/FHOkiKQ.png

--- machine-girl/pages/index.js
+++ machine-girl/pages/index.js
@@ -11,14 +11,18 @@ export default function Home() {
         </Head>
       
       <Card number="0" title="Wolf" image="https://i.imgur.com/87UHts1.png"></Card>
+      <Card number="01"  title="Machine Girl" image="https://i.imgur.com/OLGNGjB.png"></Card>
       <Card number="02" title="Mankid, the Mankid" image="https://i.imgur.com/qZRBCtV.png"></Card>
+      
+      
       <Card number="05" title="DJ Chaotic Ugly" image="https://i.imgur.com/FA2XTem.png"></Card>
       <Card number="06"  title="Sweet, the Indescribable" image="https://i.imgur.com/gJSKzJ0.jpg"></Card>
       <Card number="07" title="Reagan holiday" image="https://i.imgur.com/ycDUqPO.png"></Card>
       <Card number="08"  title="Orb Killa" image="https://i.imgur.com/PjSQyzW.jpg"></Card>
+      
       <Card number="10"  title="U-Void Synthesizer" image="https://i.imgur.com/mMZNoci.jpg"></Card>
       <Card number="11"  title="Electronic Gimp" image="https://i.imgur.com/MBv33EE.jpg"></Card>
-      <Card number="12"  title="Wolf Girl" image="https://i.imgur.com/JJSVvp8.png"></Card>
+      <Card number="12"  title="Wolf Girl" image="https://i.imgur.com/FHOkiKQ.png"></Card>
       <Card number="13" title="The Ugly Art" image="https://i.imgur.com/6y0WdUH.png"></Card>
       <Card number="14" title="Reporpoised Phantasies" image="https://i.imgur.com/zoOzFlF.png"></Card>
       <Card number="15"  title="MRK Mix Volume 2" image="https://i.imgur.com/wXEo70y.jpg"></Card>
@@ -31,13 +35,18 @@ export default function Home() {
       <Card number="21" title="Average Machine Girl Enjoyer" image="https://i.imgur.com/m7tmnwE.png"></Card>
       <Card number="22"  title="Scroll of Sorrows" image="https://i.imgur.com/gnx7Cjj.jpg"></Card>
       <Card number="23" title="Ghost" image="https://i.imgur.com/Y7t6oFO.png"></Card>
+
+
+
+      <Card number="27"  title="hounds of the hellscape" image="https://i.imgur.com/w2lDISt.png"></Card>
       <Card number="28"  title="Dog Wizard" image="https://i.imgur.com/g2RSvsq.jpg"></Card>
       <Card number="29" title="Phases of WLFGRL" image="https://i.imgur.com/2CgRmxh.png"></Card>
       <Card number="30" title="MG skateboard" image="https://i.imgur.com/HsSwYMM.png"></Card>
+
       <Card number="32"  title="MB Official" image="https://i.imgur.com/zNYHYXf.jpg"></Card>
+
       <Card number="34"  title="65,000 Year Long Rave" image="https://i.imgur.com/1nsMpsU.jpg"></Card>
-      
-      
+      <Card number="35"  title="Hellscape Tour" image="https://i.imgur.com/AhHUN3G.png"></Card>
       <Card number="36"  title="Necro Culture Vulture" image="https://i.imgur.com/ddTk568.png"></Card>
       {/* <Card number=""  title="" image=""></Card> */}
       
